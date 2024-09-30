354dcef6ac0885774fb4fb16cfe58f1793ddb896
turtle <uparupa97@icloud.com>
2024-09-30 15:25:05 +0900

    first setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Start from './pages/Start';
+import Tournament from './pages/Tournament';
+import Result from './pages/Result';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/torun" element={<Tournament />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
